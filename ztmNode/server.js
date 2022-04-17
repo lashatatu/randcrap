e146364f8c9d2f5df7d354dd2ef50c321c9e0f7f
lasha tatulashvili <lashatatu@gmail.com>
2022-04-17 06:19:55 +0400

    pm2

---
+++ ztmNode/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+function delay (duration) {
+	const startTime = Date.now();
+	while ( Date.now() - startTime < duration ) {
+
+	}
+}
+
+app.get("/", (req, res) => {
+	res.send(`Hello World: ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+	delay(4000);
+	res.send(`Timer ${process.pid}`);
+});
+
+console.log("running server");
+
+console.log("worker has started");
+app.listen(3000);
+
+
