7635a4db243c89ad662b8e0c59af1809188e6e88
lasha tatulashvili <lashatatu@gmail.com>
2022-04-26 10:40:11 +0400

    pm2

--- node/graphql/server.js
+++ node/graphql/server.js
@@ -4,14 +4,63 @@ const {graphqlHTTP} = require('express-graphql');
 
 const schema = buildSchema(`
     type Query{
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+    
+    type Product{
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!        
+    }
+    
+    type Review{
+        rating: Int!
+        comment: String!
+    }
+    
+    type Order{
+        date: String!
+        subtotal: Float!
+        items:[OrderItem]
+    }
+    
+    type OrderItem{
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-  description: 'red shoe',
-  price: 52.12,
+  products: [
+    {
+      id: 'redshoe',
+      description: 'Red shoe',
+      price: 42.12,
+    },
+    {
+      id: 'bluejean',
+      description: 'Blue Jeans',
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: '2009-01-01',
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'Old Red shoe',
+            price: 45.12,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
