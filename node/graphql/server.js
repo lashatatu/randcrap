d17a1014b06c61b406163746aac6add1b5c0ccf8
lasha tatulashvili <lashatatu@gmail.com>
2022-04-26 20:31:29 +0400

    filtering with queries

--- node/graphql/server.js
+++ node/graphql/server.js
@@ -6,32 +6,18 @@ const {loadFilesSync} = require('@graphql-tools/load-files');
 const {makeExecutableSchema} = require('@graphql-tools/schema');
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
 const schema = makeExecutableSchema({
   typeDefs: typesArray,
-  resolvers: {
-    Query: {
-      products:async (parent) => {
-        const product=await Promise.resolve(parent.products)
-        return product
-      },
-      orders: (parent) => {
-        return parent.orders
-      },
-    },
-  },
-});
+  resolvers: resolversArray,
 
-const root = {
-  products: require('./products/products.model'),
-  orders: require('./orders/orders.model'),
-};
+});
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 
