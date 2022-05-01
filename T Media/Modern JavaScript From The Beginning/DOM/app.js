28058823e90baf8d7a26eaa5bb1cc309837d6e9f
lasha tatulashvili <lashatatu@gmail.com>
2022-05-01 14:09:59 +0400

    moving old files to Old folder

--- T Media/Modern JavaScript From The Beginning/DOM/app.js
+++ T Media/Modern JavaScript From The Beginning/DOM/app.js
@@ -1,28 +1,25 @@
-// document.querySelector('.card-title')
-//   .addEventListener('click', function () {
-//     console.log('card-title');
-//   });
-//
-// document.querySelector('.card-content')
-//   .addEventListener('click', function () {
-//     console.log('card-content');
-//   });
-//
-// document.querySelector('.card')
-//   .addEventListener('click', function () {
-//     console.log('card');
-//   });
-//
-// document.querySelector('.col')
-//   .addEventListener('click', function () {
-//     console.log('col');
-//   });
+document.querySelector('form').addEventListener('submit', function (e) {
+  const task = document.getElementById('task').value;
 
-document.body.addEventListener('click', deleteItem);
+  let tasks;
 
-function deleteItem (e) {
-  if ( e.target.parentElement.classList.contains('delete-item') ) {
-    console.log('delete item');
-    e.target.parentElement.parentElement.remove();
+  if ( localStorage.getItem('tasks') === null ) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
   }
-}
+
+  tasks.push(task)
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  alert('task saved');
+
+  e.preventDefault();
+});
+
+
+const tasks=JSON.parse(localStorage.getItem('tasks'))
+
+tasks.forEach((task)=>{
+  console.log(task);
+})
