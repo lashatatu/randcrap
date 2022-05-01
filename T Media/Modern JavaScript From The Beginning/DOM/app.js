83ac3d78cd53720594a931c2e2e961b90159de88
lasha tatulashvili <lashatatu@gmail.com>
2022-05-01 13:42:44 +0400

    moving old files to Old folder

--- T Media/Modern JavaScript From The Beginning/DOM/app.js
+++ T Media/Modern JavaScript From The Beginning/DOM/app.js
@@ -1,9 +1,26 @@
-const btn=document.querySelector('#btn');
+const btn = document.querySelector('#btn');
+const name = document.querySelector('.name');
+const textInput = document.querySelector('#textInput');
+
+textInput.addEventListener('keydown',runEvent);
+
+function runEvent (e) {
+  if ( e.keyCode === 13 ) {
+    name.innerHTML = textInput.value;
+    textInput.value = '';
+  }
+
+  if ( e.keyCode === 27 ) {
+    textInput.value = '';
+  }
+
+  name.innerText = e.target.value;
+};
 
 console.log(btn);
-btn.addEventListener('click',()=>{
+btn.addEventListener('click', () => {
   // switch body style
-  btn.style.backgroundColor=btn.style.backgroundColor==='white'?'#ccc':'white';
-  btn.style.textDecoration=btn.style.textDecoration==='none'?'underline':'none';
+  btn.style.backgroundColor = btn.style.backgroundColor === 'white' ? '#CCCCCC' : 'white';
+  btn.style.textDecoration = btn.style.textDecoration === 'none' ? 'underline' : 'none';
 
 });
