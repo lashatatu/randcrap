7b6ff21daf37a34f5f4d129b2679d0e7787bd73f
lasha tatulashvili <lashatatu@gmail.com>
2022-05-01 22:04:36 +0400

    number guesser

--- T Media/Modern JavaScript From The Beginning/DOM/Loan Calculator/app.js
+++ T Media/Modern JavaScript From The Beginning/DOM/Loan Calculator/app.js
@@ -1,7 +1,13 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 function calculateResults (e) {
-
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -20,24 +26,26 @@ function calculateResults (e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
+
+
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 function showError (error) {
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
   const errorDiv = document.createElement('div');
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
 
   errorDiv.className = 'alert alert-danger';
-
   errorDiv.appendChild(document.createTextNode(error));
-
   card.insertBefore(errorDiv, heading);
-
   setTimeout(clearError, 3000);
 }
 
