bbd336fd5eeb8c757171e77528cf58f2bda3c388
lasha tatulashvili <lashatatu@gmail.com>
2022-05-01 14:44:04 +0400

    fixed bug

--- T Media/Modern JavaScript From The Beginning/DOM/Task List/app.js
+++ T Media/Modern JavaScript From The Beginning/DOM/Task List/app.js
@@ -8,27 +8,63 @@ loadEventListeners();
 
 function loadEventListeners () {
   form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask (e) {
 
   if ( taskInput.value === '' ) {
     alert('add task');
+  }else{
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+
+    link.innerHTML = '<a class="fa fa-remove"></a>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+
+    taskInput.value = '';
   }
-
-  const li = document.createElement('li');
-  li.className = 'collection-item';
-
-  li.appendChild(document.createTextNode(taskInput.value));
-
-  const link = document.createElement('a');
-  link.className = 'delete-item secondary-content';
-
-  link.innerHTML = '<a class="fa fa-remove"></a>';
-  li.appendChild(link);
-
-  taskList.appendChild(li);
-
-  taskInput.value = '';
   e.preventDefault();
+
+
+}
+
+function removeTask (e) {
+  if ( e.target.parentElement.classList.contains('delete-item') ) {
+    if ( confirm('are you sure') ) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks (e) {
+  while ( taskList.firstChild ) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks (e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    (task) => {
+      const item = task.firstChild.textContent;
+      if ( item.toLowerCase().indexOf(text) !== -1 ) {
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+
+      }
+
+    },
+  );
 }
