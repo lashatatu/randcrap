79a90b1eab806fa42a8301e980d4219a2d1d1c57
lasha tatulashvili <lashatatu@gmail.com>
2022-05-02 17:35:44 +0400

    number guesser 4

--- T Media/Modern JavaScript From The Beginning/DOM/Number Guesser/app.js
+++ T Media/Modern JavaScript From The Beginning/DOM/Number Guesser/app.js
@@ -1,6 +1,6 @@
 let min = 1;
 let max = 10;
-let winningNum = 2;
+let winningNum = getRandomNum(min, max);
 let guessesLeft = 3;
 
 const game = document.querySelector('#game');
@@ -13,6 +13,11 @@ const message = document.querySelector('.message');
 minNum.textContent = min;
 maxNum.textContent = max;
 
+game.addEventListener('mousedown', function (e) {
+  if ( e.target.className === 'play-again' ) {
+    window.location.reload();
+  }
+});
 guessBtn.addEventListener('click', function () {
   let guess = parseInt(guessInput.value);
 
@@ -47,6 +52,13 @@ function gameOver (won, msg) {
   guessInput.style.borderColor = 'green';
   message.style.color = color;
   setMessage(msg);
+
+  guessBtn.value = 'play again';
+  guessBtn.className += 'play-again';
+}
+
+function getRandomNum (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function setMessage (msg, color) {
