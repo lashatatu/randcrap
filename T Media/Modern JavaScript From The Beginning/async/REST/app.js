ad0567499ea6683907b1dcd4c78a5071ef111887
lasha tatulashvili <lashatatu@gmail.com>
2022-05-10 07:11:56 +0400

    Github User project

--- T Media/Modern JavaScript From The Beginning/async/REST/app.js
+++ T Media/Modern JavaScript From The Beginning/async/REST/app.js
@@ -15,7 +15,7 @@ const data = {
 // http.post('https://jsonplaceholder.typicode.com/users', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
-//
+// //
 // http.put('https://jsonplaceholder.typicode.com/users/2', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
