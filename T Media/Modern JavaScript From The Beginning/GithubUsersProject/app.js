02c8ee9244ffb2124e5a43e4c7e3cb999595e917
lasha tatulashvili <lashatatu@gmail.com>
2022-05-10 08:46:46 +0400

    Github finder 2

--- T Media/Modern JavaScript From The Beginning/GithubUsersProject/app.js
+++ T Media/Modern JavaScript From The Beginning/GithubUsersProject/app.js
@@ -1,21 +1,21 @@
 // search input
-const github=new Github
-const ui=new UI
+const github = new Github;
+const ui = new UI;
 
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup', (e) => {
   const userText = e.target.value.toLowerCase();
-  if(userText!==''){
+  if ( userText !== '' ) {
     github.getUser(userText).then(data => {
-      if(data.profile.message==='Not Found'){
-
-      }else{
+      if ( data.profile.message === 'Not Found' ) {
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
         ui.showProfile(data.profile);
-
+        ui.showRepos(data.repos);
       }
-    })
-  }else {
-    document.getElementById('profile').innerHTML='';
+    });
+  } else {
+    ui.clearProfile();
   }
-})
+});
--- T Media/Modern JavaScript From The Beginning/GithubUsersProject/ui.js
+++ T Media/Modern JavaScript From The Beginning/GithubUsersProject/ui.js
@@ -29,6 +29,54 @@ class UI {
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
       `;
-    console.log(user);
+  }
+
+  showRepos (repos) {
+    let output = '';
+
+    repos.forEach(repo => {
+      output += `
+      
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge bg-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge bg-primary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge bg-primary">Forks: ${repo.forms_count}</span>
+          </div>
+        
+        </div>
+      </div>
+      `
+    })
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  showAlert (message, className) {
+    this.clearAlert();
+    const div = document.createElement('div');
+    div.className = className;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.searchContainer');
+    const search = document.querySelector('.search');
+    container.insertBefore(div, search);
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert(){
+    const currentAlert=document.querySelector('.alert');
+    if(currentAlert){
+      currentAlert.remove();
+    }
+  }
+
+  clearProfile () {
+    this.profile.innerHTML = '';
   }
 }
