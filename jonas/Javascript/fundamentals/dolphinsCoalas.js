190eaf2f7478ff548834c0409bee3b9b03d454b0
lasha tatulashvili <lashatatu@gmail.com>
2022-05-17 17:26:58 +0400

    else if

---
+++ jonas/Javascript/fundamentals/dolphinsCoalas.js
@@ -0,0 +1,17 @@
+const dolphinsScore1 = 96;
+const dolphinsScore2 = 108;
+const dolphinsScore3 = 89;
+const dolphinsScoreSum = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3)/3;
+
+const koalasScore1 = 88;
+const koalasScore2 = 105;
+const koalasScore3 = 110;
+const koalasScoreSum = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
+
+if ( dolphinsScoreSum >= 100 && dolphinsScoreSum > koalasScoreSum ) {
+  console.log('dolphins won');
+} else if (koalasScoreSum >= 100 && koalasScoreSum > dolphinsScoreSum ) {
+  console.log('koalas won');
+} else if ( koalasScore1 ) {
+  console.log('draw');
+}
