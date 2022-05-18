2dddf3ea94d3118142a7e3cd884d4acb8c2be3ce
lasha tatulashvili <lashatatu@gmail.com>
2022-05-18 14:20:07 +0400

    tip value

--- jonas/Javascript/fundamentals/tipValueTut.js
+++ jonas/Javascript/fundamentals/tipValueTut.js
@@ -1,4 +1,5 @@
-const bil = 430;
-const tip = bil > 50 && bil < 300 ? bil / 100 * 15 : bil / 100 * 20;
+const bil = 40;
+const tip = bil >= 50 && bil <= 300 ? bil / 100 * 15 : bil / 100 * 20;
+const finalBil=tip+bil
 
-console.log(tip);
+console.log(finalBil);
