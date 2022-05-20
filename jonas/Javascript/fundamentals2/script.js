83a53f5d672b36fdd98c4d48046fa262462c48e4
lasha tatulashvili <lashatatu@gmail.com>
2022-05-20 07:31:37 +0400

    functions 2

--- jonas/Javascript/fundamentals2/script.js
+++ jonas/Javascript/fundamentals2/script.js
@@ -1,9 +1,25 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if(passTest) hasDriversLicense=true;
+// if(hasDriversLicense) {
+//   console.log('i can drive')
+// }
 
-if(passTest) hasDriversLicense=true;
-if(hasDriversLicense) {
-  console.log('i can drive')
-}
+// function logger () {
+//   console.log('my name is Lasha');
+// }
+//
+// logger();
+//
+// function fruitProcessor (apples, oranges) {
+//   console.log(apples, oranges);
+//
+//   return `juice with ${apples} apples and ${oranges} oranges`
+// }
+//
+// console.log(fruitProcessor(2,3))
+//
+// console.log(fruitProcessor(2,4));
