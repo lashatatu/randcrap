95b87f0b385cb592b625e34f98fc61e8f1e1a03e
lasha tatulashvili <lashatatu@gmail.com>
2022-05-24 20:41:18 +0400

    open close

--- jonas/Javascript/modalProject/script.js
+++ jonas/Javascript/modalProject/script.js
@@ -7,7 +7,18 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
-
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 for ( let i = 0; i < btnsOpenModal.length; i++ ) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
