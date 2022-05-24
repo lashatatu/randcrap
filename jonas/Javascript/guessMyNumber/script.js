297f05e6a7b7cb229a00821e6a9be083dad727e3
lasha tatulashvili <lashatatu@gmail.com>
2022-05-24 07:29:11 +0400

    dom css

--- jonas/Javascript/guessMyNumber/script.js
+++ jonas/Javascript/guessMyNumber/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check')
   .addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value);
@@ -7,7 +12,28 @@ document.querySelector('.check')
 
     if ( !guess ) {
       document.querySelector('.message').textContent = 'No Number 😭';
+    } else if ( guess === secretNumber ) {
+      document.querySelector('.message').textContent = 'Correct Number ✔';
+    } else if ( guess > secretNumber ) {
+      if ( score > 1 ) {
+        document.querySelector('.message').textContent = 'Number is too High';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You lost';
+        document.querySelector('.score').textContent = 0;
+      }
+    } else if ( guess <= secretNumber ) {
+      if ( score > 1 ) {
+        document.querySelector('.message').textContent = 'Number is too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You lost';
+        document.querySelector('.score').textContent = 0;
+      }
     }
+
   });
 
 // text
