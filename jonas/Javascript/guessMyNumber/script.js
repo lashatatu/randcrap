5399549fecb0cc86a3f5701d914e7c0e080cfd27
lasha tatulashvili <lashatatu@gmail.com>
2022-05-24 07:10:29 +0400

    click

--- jonas/Javascript/guessMyNumber/script.js
+++ jonas/Javascript/guessMyNumber/script.js
@@ -1,7 +1,15 @@
 'use strict';
 
-document.querySelector('.message').textContent;
-document.querySelector('.guess').value = 23;
+document.querySelector('.check')
+  .addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
 
+    if ( !guess ) {
+      document.querySelector('.message').textContent = 'No Number 😭';
+    }
+  });
+
+// text
 
 
