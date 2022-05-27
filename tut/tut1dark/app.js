c6a81d3486c841d5c276e6904414be9862f8e581
lasha tatulashvili <lashatatu@gmail.com>
2022-05-27 14:44:58 +0400

    pig game done

---
+++ tut/tut1dark/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const click = document.querySelector('.click');
+const body = document.querySelector('body');
+
+click.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
