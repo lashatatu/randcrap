54bf72c81f046ee530b8858d1b5c642a7f7bc762
lasha tatulashvili <lashatatu@gmail.com>
2022-06-01 17:19:28 +0400

    functions start

---
+++ jonas/Javascript/functions/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers, price) {
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('lh123');
