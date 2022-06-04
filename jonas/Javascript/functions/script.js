3074c26e1d296fbe62d3b80a2e93fb73db545e3e
lasha tatulashvili <lashatatu@gmail.com>
2022-06-04 19:17:11 +0400

    functions start

--- jonas/Javascript/functions/script.js
+++ jonas/Javascript/functions/script.js
@@ -16,15 +16,57 @@
 //
 // createBooking('lh123');
 
-const greet=(greeting)=>{
-  return (name)=>{
-    console.log(`${greeting} ${name}`);
-  }
-}
+// const greet=(greeting)=>{
+//   return (name)=>{
+//     console.log(`${greeting} ${name}`);
+//   }
+// }
+//
+// const greeterHey=greet('hey')
+//
+// greeterHey('lasha')
+// greeterHey('nana')
+//
+// greet('hello')('lasha')
 
-const greeterHey=greet('hey')
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book (flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+    this.bookings.push({flight: `${this.iataCode} ${flightNum} ,name`});
+  },
+};
 
-greeterHey('lasha')
-greeterHey('nana')
+lufthansa.book(239, 'lasha tatulashvili');
+console.log(lufthansa);
 
-greet('hello')('lasha')
+const eurowings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// book(23,'lt')
+book.call(eurowings, 23, 'la');
+
+const bookEW = book.bind(eurowings);
+bookEW(23, 'steven');
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  this.planes++;
+  console.log(this.planes);
+};
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+const addTaxRate = (rate) => (value) => {
+  return value + value * rate;
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
