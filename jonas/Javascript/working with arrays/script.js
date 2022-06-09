fbecc48148c5d1a7fc9944c4ff03ce1d91eacc95
lasha tatulashvili <lashatatu@gmail.com>
2022-06-09 19:52:41 +0400

    reduce

--- jonas/Javascript/working with arrays/script.js
+++ jonas/Javascript/working with arrays/script.js
@@ -106,5 +106,15 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+const deposits=movements.filter((mov)=>{
+  return mov>0
+})
+const withdrawals=movements.filter((mov)=>{
+  return mov<0
+})
+
+console.log(deposits);
+console.log(withdrawals);
+
 
 
