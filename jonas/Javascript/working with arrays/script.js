1438e91ea875a43c41e77834a40198593a085e6c
lasha tatulashvili <lashatatu@gmail.com>
2022-06-12 07:55:44 +0400

    filling arrays

--- jonas/Javascript/working with arrays/script.js
+++ jonas/Javascript/working with arrays/script.js
@@ -61,10 +61,13 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = (movements) => {
+const displayMovements = (movements, sort = false) => {
   containerMovements.innerHTML = ``;
 
-  movements.forEach((mov, i) => {
+  const movs = sort ? movements.slice()
+    .sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
         <div class="movements__row">
@@ -173,6 +176,13 @@ btnClose.addEventListener('click', (e) => {
 
 });
 
+let sorted = false;
+
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -185,11 +195,3 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-console.log(movements);
-
-const anyDeposits = movements.some(mov => mov > 0);
-console.log(anyDeposits);
-
-console.log(account4.movements.every(mov => mov > 0));
-
-const deposit = mov => mov > 0;
