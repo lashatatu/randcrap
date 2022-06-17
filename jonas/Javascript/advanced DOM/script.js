5075c66629644ede6565f0c8c1aa88b998f07267
lasha tatulashvili <lashatatu@gmail.com>
2022-06-17 14:24:44 +0400

    how dom really works

--- jonas/Javascript/advanced DOM/script.js
+++ jonas/Javascript/advanced DOM/script.js
@@ -13,10 +13,13 @@ const openModal = function () {
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = function (e) {
+  e.preventDefault()
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+btnsOpenModal.forEach(btn=>btn.addEventListener('click', openModal))
 
 for ( let i = 0; i < btnsOpenModal.length; i++ ) btnsOpenModal[i].addEventListener('click', openModal);
 
