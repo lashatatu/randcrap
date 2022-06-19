e42041b183691a95320da67653ab027b5847037a
lasha tatulashvili <lashatatu@gmail.com>
2022-06-19 16:14:39 +0400

    sticky navigation

--- jonas/Javascript/advanced DOM/script.js
+++ jonas/Javascript/advanced DOM/script.js
@@ -7,8 +7,16 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+// Scroll
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+
+// Tabs
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -65,24 +73,44 @@ document.querySelector('.nav__links')
 
 // Tabbed Component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', (e) => {
   const clicked = e.target.closest('.operations__tab');
 
   if ( !clicked ) {
     return;
   }
-  tabs.forEach(t=>t.classList.remove('operations__tab--active'))
-  tabsContent.forEach(t=>t.classList.remove('operations__content--active'))
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
 
   clicked.classList.add('operations__tab--active');
 
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList
+    .add('operations__content--active');
 
 });
+
+// Menu fade animation
+
+const handleHover=function (e){
+  if ( e.target.classList.contains('nav__link') ) {
+    const link = e.target;
+    const siblings = link.closest('.nav')
+      .querySelectorAll('.nav__link');
+    const logo = link.closest('.nav')
+      .querySelector('img');
+
+    siblings.forEach(el => {
+        if ( el !== link ) el.style.opacity =this;
+      },
+    );
+    logo.style.opacity = this;
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
