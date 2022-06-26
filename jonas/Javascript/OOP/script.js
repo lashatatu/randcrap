4ded8ae52f3c5c9960e20d692a6608832478dd26
lasha tatulashvili <lashatatu@gmail.com>
2022-06-26 20:30:54 +0400

    prototypes

--- jonas/Javascript/OOP/script.js
+++ jonas/Javascript/OOP/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  console.log(firstName);
+  console.log(birthYear);
+};
+
+const jonas = new Person('Jonas', 1990);
+const lasha = new Person('Lasha', 1988);
+
+console.log(jonas);
+console.log(lasha.age);
+
+
+
