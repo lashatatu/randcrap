a0863fe40d313ee6f7448371e0ae966dc8397ada
lasha tatulashvili <lashatatu@gmail.com>
2022-06-27 22:05:19 +0400

    es6 classes

--- jonas/Javascript/OOP/script.js
+++ jonas/Javascript/OOP/script.js
@@ -12,7 +12,27 @@ const jonas = new Person('Jonas', 1990);
 const lasha = new Person('Lasha', 1988);
 
 console.log(jonas);
-console.log(lasha.age);
+console.log(lasha);
 
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
 
+jonas.calcAge();
 
+console.log(jonas.__proto__.__proto__);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+
+console.log(arr.__proto__);
+
+arr.sort((a, b) => {
+  return a - b;
+});
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+const ss = new Set(arr);
+
+console.log(ss);
