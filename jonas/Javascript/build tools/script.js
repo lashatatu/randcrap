efa887689261aacb7ba1cd363fea6c1042a73304
lasha tatulashvili <lashatatu@gmail.com>
2022-07-07 03:30:51 +0400

    import exports

---
+++ jonas/Javascript/build tools/script.js
@@ -0,0 +1,8 @@
+import './shoppingCart.js'
+import { shippingCost, addToCart } from './shoppingCart.js';
+console.log('import');
+console.log(shippingCost);
+addToCart()
+
+
+
---
+++ jonas/Javascript/build tools/shoppingCart.js
@@ -0,0 +1,8 @@
+console.log('exporting');
+
+export const shippingCost = 10;
+export const cart = [];
+//lasha
+export const addToCart=function (){
+  console.log('lasha');
+}
