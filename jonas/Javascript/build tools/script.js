162f7b93d6d08f82ae0cd8e843147bb61b726f87
lasha tatulashvili <lashatatu@gmail.com>
2022-07-07 06:58:51 +0400

    import exports

--- jonas/Javascript/build tools/script.js
+++ jonas/Javascript/build tools/script.js
@@ -1,8 +1,9 @@
-import './shoppingCart.js'
-import { shippingCost, addToCart } from './shoppingCart.js';
-console.log('import');
-console.log(shippingCost);
-addToCart()
+const getData = async () => {
+  const req = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await req.json();
+  data.map(({id,title})=>{
+    return console.log(`id: ${id} title: ${title}`)
+  })
+};
 
-
-
+await getData()
--- jonas/Javascript/build tools/shoppingCart.js
+++
@@ -1,8 +0,0 @@
-console.log('exporting');
-
-export const shippingCost = 10;
-export const cart = [];
-//lasha
-export const addToCart=function (){
-  console.log('lasha');
-}
