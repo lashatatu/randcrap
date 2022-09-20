9716db739a9688f04508ae4c4f0197b217d5869f
lasha <lashatatu@gmail.com>
2022-09-21 03:37:18 +0400

    typescript start 5

--- typescript/start/using-ts.ts
+++ typescript/start/using-ts.ts
@@ -7,5 +7,10 @@ function add(num1: number, num2: number) {
 }
 
 button.addEventListener("click", function () {
-  console.log(add(input1.value, input2.value));
+  // const h1 = document.createElement("h1");
+  // h1.innerText = add(+input1.value, +input2.value).toString();
+  // document.body.appendChild(h1)
+  console.log(add(+input1.value, +input2.value));
 });
+
+//text
