bfc56328a255739c8c3497c6ec443dcd3fb19da3
lasha <lashatatu@gmail.com>
2022-11-28 17:27:47 +0400

    HTML CSS course start

---
+++ typescript/start/using-ts.js
@@ -0,0 +1,13 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    var h1 = document.createElement("h1");
+    h1.innerText = add(+input1.value, +input2.value).toString();
+    document.body.appendChild(h1);
+    console.log(add(+input1.value, +input2.value));
+});
+//text
