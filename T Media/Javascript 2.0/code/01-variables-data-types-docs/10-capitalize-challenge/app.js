da6b5a748115c6d430048ea10f7b6b07f636c8df
lasha <lashatatu@gmail.com>
2023-04-09 04:04:59 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 18

--- T Media/Javascript 2.0/code/01-variables-data-types-docs/10-capitalize-challenge/app.js
+++ T Media/Javascript 2.0/code/01-variables-data-types-docs/10-capitalize-challenge/app.js
@@ -1,4 +1,4 @@
-const myString = 'developer';
-const myNewString=myString[0].toUpperCase()+myString.slice(1);
+const myString = "developer";
+const myNewString = myString[0].toUpperCase() + myString.slice(1);
 
 console.log(myNewString);
