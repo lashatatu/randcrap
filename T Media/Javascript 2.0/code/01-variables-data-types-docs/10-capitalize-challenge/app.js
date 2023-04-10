bd4bf499641f696ccb6bda827b8f4f0fa17f4347
lasha <lashatatu@gmail.com>
2023-04-10 04:49:53 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 23

--- T Media/Javascript 2.0/code/01-variables-data-types-docs/10-capitalize-challenge/app.js
+++
@@ -1,4 +0,0 @@
-const myString = "developer";
-const myNewString = myString[0].toUpperCase() + myString.slice(1);
-
-console.log(myNewString);
---
+++ T Media/Javascript 2.0/code/01-variables-data-types-docs/13-number-challenge/app.js
@@ -0,0 +1,13 @@
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+let sumOutput = x + y;
+let differenceOutput = x - y;
+let productOutput = x * y;
+let quotientOutput = x / y;
+let rmOutput = x % y;
+console.log(x+" + "+y+" = "+sumOutput); // 31 + 15 = 46
+console.log(x+" - "+y+" = "+differenceOutput); // 31 - 15 = 16
+console.log(x+" * "+y+" = "+productOutput); // 31 * 15 = 465
+console.log(x+" / "+y+" = "+quotientOutput); // 31 / 15 = 2.066666666666667
+console.log(x+" % "+y+" = "+rmOutput); // 31 % 15 = 1
+console.log(x);
