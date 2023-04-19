5079891f73a845cf90616c3b7b65e389594f7d26
lasha <lashatatu@gmail.com>
2023-04-19 16:47:17 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 67

--- T Media/Javascript 2.0/code/05-iteration-array-methods-docs/04-fizzbuzz-challenge/app.js
+++ T Media/Javascript 2.0/code/05-iteration-array-methods-docs/04-fizzbuzz-challenge/app.js
@@ -1,17 +1,29 @@
-for (let i=1;i<=100;i++){
-  if(i%5===0&&i%3===0){
-    console.log('FizzBuzz');
-    continue;
-  }
+const companies = [
+  { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+  { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+  { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+  { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+  { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+  { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+  { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+  { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+  { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+];
 
-  if(i%3===0){
-    console.log('Fizz');
-    continue;
-  }
+const filteredCompany=companies.filter(company=>company.end-company.start>=10)
 
-  if(i%5===0){
-    console.log('Buzz');
-    continue;
-  }
-  console.log(i);
-}
+
+
+// console.log(filteredCompany);
+
+const cart = [
+  { name: 'Prooduct 1', price: 130 },
+  { name: 'Product 2', price: 150 },
+  { name: 'Product 3', price: 175 },
+];
+
+const total=cart.reduce((acc,cur)=>{
+  return acc+cur.price
+},0)
+
+console.log(total);
