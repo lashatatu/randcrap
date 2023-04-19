92930b06b8ca96155321f50446e099b2f3c26a76
lasha <lashatatu@gmail.com>
2023-04-19 18:52:44 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 68

--- jonas/Javascript/build tools/script.js
+++
@@ -1,9 +0,0 @@
-const getData = async () => {
-  const req = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await req.json();
-  data.map(({id,title})=>{
-    return console.log(`id: ${id} title: ${title}`)
-  })
-};
-
-await getData()
--- jonas/Javascript/fundamentals/tipValueTut.js
+++
@@ -1,5 +0,0 @@
-const bil = 40;
-const tip = bil >= 50 && bil <= 300 ? bil / 100 * 15 : bil / 100 * 20;
-const finalBil=tip+bil
-
-console.log(finalBil);
