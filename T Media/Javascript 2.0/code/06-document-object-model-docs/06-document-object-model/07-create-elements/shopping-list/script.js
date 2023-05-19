781425c1258263d30c06ad6070cc6531067ccbcb
lasha <lashatatu@gmail.com>
2023-05-19 13:41:33 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 76

--- T Media/Javascript 2.0/code/06-document-object-model-docs/06-document-object-model/07-create-elements/shopping-list/script.js
+++ T Media/Javascript 2.0/code/06-document-object-model-docs/06-document-object-model/07-create-elements/shopping-list/script.js
@@ -1,13 +1,10 @@
-const div = document.createElement('div');
-div.className = 'my-element';
-div.id = 'my-element';
-div.setAttribute('title', 'My Element');
+const div = document.createElement("div");
+div.className = "my-element";
+div.id = "my-element";
+div.setAttribute("title", "My Element");
 
-// div.innerText = 'Hello World';
+const text = document.createTextNode("Hello World");
+div.appendChild(text)
+const items= document.querySelector("#item-list");
 
-const text = document.createTextNode('Hello World');
-div.appendChild(text);
-
-// document.body.appendChild(div);
-
-document.querySelector('ul').appendChild(div);
+items.appendChild(div);
