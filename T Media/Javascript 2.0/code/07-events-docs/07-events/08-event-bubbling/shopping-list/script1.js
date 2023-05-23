c979b42ba1e8c5ef15362f1b779de0a998e82562
lasha <lashatatu@gmail.com>
2023-05-23 13:21:52 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 92

---
+++ T Media/Javascript 2.0/code/07-events-docs/07-events/08-event-bubbling/shopping-list/script1.js
@@ -0,0 +1,5 @@
+const button= document.querySelector('form button');
+const div=document.querySelector('form div:nth-child(2)')
+button.addEventListener('click', () => {
+  alert('You clicked me!')
+})
