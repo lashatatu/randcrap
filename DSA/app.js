c945b87e55dc3d29a17740cf13af576590f5a0a7
lasha <lashatatu@gmail.com>
2023-09-16 14:53:26 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 114
    
    DSA
    
    Container With Most Water

--- DSA/app.js
+++ DSA/app.js
@@ -1,22 +1,64 @@
-// Container With Most Water
-let maxArea = function (height) {
-  let acc=0
-  for (let i = 0; i < height.length; i++) {
-    for (let j = 0; j < height.length; j++) {
-      if(height[i]>height[j]) {
-        let accI=height[j]
-        const calculate = accI * [j - i];
-        if ( acc < calculate ) {
-          acc = calculate
-        }
-      }
-    }
-  }
-  return acc;
-};
 
-maxArea([1,8,6,2,5,4,8,3,7]);
-maxArea([1, 1]);
+
+/*
+
+ // Container With Most Water
+ let maxArea = function (heights) {
+ let p1 = 0;
+ let p2 = heights.length - 1;
+ let maxArea = 0;
+ while (p1< p2) {
+ const height = Math.min(heights[p1], heights[p2]);
+ const width=p2-p1
+ const area=height*width;
+ maxArea=Math.max(maxArea,area)
+ if(heights[p1]<=heights[p2]){
+ p1++;
+ }else {
+ p2--
+ }
+ }
+ return maxArea
+ };
+
+ maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+ maxArea([1, 1]);
+ */
+
+//////////////////////////////////////////////////////////////////
+
+/*
+
+ // Container With Most Water My solution
+
+ let maxArea = function (height) {
+ let acc=0
+ for (let i = 0; i < height.length; i++) {
+ for (let j = i+1; j < height.length; j++) {
+ if(height[i]>=height[j]) {
+ let accI=height[j]
+ const calculate = accI * [j - i];
+ if ( acc < calculate ) {
+ acc = calculate
+ }
+ }
+ if(height[i]<=height[j]) {
+ let accJ=height[i]
+ const calculate = accJ * [j - i];
+ if ( acc < calculate ) {
+ acc = calculate
+ }
+ }
+ }
+ }
+ return acc;
+ };
+
+ maxArea([1,8,6,2,5,4,8,3,7]);
+ maxArea([1, 1]);
+
+ */
+
 // TWO SUM
 /*
  let twoSum = function (nums, target) {
