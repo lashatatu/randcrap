517a4e55614d7965675ee91d5034dff92d455502
lasha <lashatatu@gmail.com>
2024-03-29 20:02:28 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 114
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    javascript 30 drum kit done

--- 100DaysOfCodeAcademymind/Day1-Day5-BuildingFirstWebsite/script.js
+++ 100DaysOfCodeAcademymind/Day1-Day5-BuildingFirstWebsite/script.js
@@ -1,2 +1,11 @@
-let h1=document.querySelector('h1');
+const input=document.querySelector("input");
+const testbg = document.querySelector(".test-bg");
+
+testbg.addEventListener("change", function () {
+  // change colors on click
+  input.value
+    ? (testbg.style.backgroundColor = input.value)
+    : (testbg.style.backgroundColor = "white");
+});
+
 
