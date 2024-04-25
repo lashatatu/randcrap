14b1aae436ddce9c755ab7499f7562c5d6cb0aaf
lasha <lashatatu@gmail.com>
2024-04-25 17:22:24 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 114
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    javascript 30 clock done

--- ReactTut/src/App.jsx
+++ ReactTut/src/App.jsx
@@ -1,43 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-// class Test extends React.Component {
-//   state = {};
-//   //
-//   hancleClick() {
-//     this.setState({ a: 1 },()=>this.setState({b:1}));
-//     // this.setState({ b: 2 });
-//     // this.setState({ n: 3 });
-//   }
-//
-//   render() {
-//     // let langs=['asd','asd11','asdasdasd']
-//     console.log(this.state);
-//     return <button onClick={()=>this.hancleClick()}>click</button>
-//   }
-// }
-// export default Test
+const Count = () => {
+  const [count, setCount] = useState(0);
 
-// function mycomponent({n}){
-//   const [num, setNum] = useState(n);
-//
-//   useEffect(() => {
-//     console.log(num);
-//     setNum(num+1)
-//     return ()=>{
-//       console.log(num);
-//     }
-//   }, );
-//   return <strong>{num}</strong>
-// }
-//
-// export default mycomponent()
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
-// function Count(){
-//   const [{ count1, count2 }, setCount] = useState({ count1:10,count2:20});
-//   const handleClick=()=>setCount((curremtState)=>({...curremtState,count2: curremtState.count2+5}))
-//   return <button onClick={handleClick}>increase {count2}c2 {count1}c1 </button>
-// }
-//
-// export default Count
+  return (
+    <div>
+      <p>
+        You clicked {count} {count <= 1 ? "time" : "times"}
+      </p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
 
-
+export default Count;
