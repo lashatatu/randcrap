a2e1771e14f23a6f63bffafba5cb05c386b012a9
lasha <lashatatu@gmail.com>
2024-04-25 17:13:52 +0400

    17. Backgrounds, Gradients, & Filters
    Traversy 114
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    javascript 30 clock done

---
+++ rand/script.js
@@ -0,0 +1,9 @@
+// const watermarkText = 'Watermark Text';
+// const numberOfWatermarks = 50; // Adjust this number as needed
+//
+// for (let i = 0; i < numberOfWatermarks; i++) {
+//   const watermark = document.createElement('div');
+//   watermark.className = 'watermark';
+//   watermark.textContent = watermarkText;
+//   document.body.appendChild(watermark);
+// }
