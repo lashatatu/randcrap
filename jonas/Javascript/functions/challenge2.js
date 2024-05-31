fa707778a12c74a1a28c02fb5ef35bcc81764c81
lasha <lashatatu@gmail.com>
2024-05-31 05:54:04 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    javascript 30 clock done
    
    Perfect solution for small business done

--- jonas/Javascript/functions/challenge2.js
+++ jonas/Javascript/functions/challenge2.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 (function () {
-  const header = document.querySelector('h1');
-  header.style.color = 'red';
+  const header = document.querySelector("h1");
+  header.style.color = "red";
 
-  document.querySelector('body').addEventListener('click',function (){
-    header.style.color='blue'
-  })
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
 })();
