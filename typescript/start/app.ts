684804fec5f91a50788876cdd9c98bb7b4313567
lasha <lashatatu@gmail.com>
2024-06-05 00:09:51 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 14
    
    Perfect solution for small business done

--- typescript/start/app.ts
+++ typescript/start/app.ts
@@ -1,3 +1,17 @@
-console.log('lasha')
-console.log('asdadsa')
-console.log('asdadsa')
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+
+  const result=n1+n2;
+  if (showResult) {
+    console.log(phrase+result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.5;
+
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
