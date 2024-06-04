aecf2477dec15018d2ed6d827e46403baf528546
lasha <lashatatu@gmail.com>
2024-06-05 01:25:46 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 18
    
    Perfect solution for small business done

--- T Media/Javascript 2.0/code/17-modules-and-tooling-docs/17-modules-and-tooling/11-tacalorie-webpack-refactor/tracalorie-webpack/webpack.config.js
+++ T Media/Javascript 2.0/code/17-modules-and-tooling-docs/17-modules-and-tooling/11-tacalorie-webpack-refactor/tracalorie-webpack/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/basics.js',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
