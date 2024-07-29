464931c877d416349837469200081597dd76c4bf
lasha <lashatatu@gmail.com>
2024-07-29 19:41:58 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114 - 10 - 05
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 52
    
    Perfect solution for small business done

--- typescript/next-gen-01-starting-setup/src/app.ts
+++ typescript/next-gen-01-starting-setup/src/app.ts
@@ -1,2 +1,10 @@
-const userName = "max";
-'sd'
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
