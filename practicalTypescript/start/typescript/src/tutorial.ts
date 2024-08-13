0d70c83b9dbfbded9fdcefddee3dadaa18ea22a4
lasha <lashatatu@gmail.com>
2024-08-13 22:06:37 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114 - 10 - 05
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 55
    
    Practical Typescript Fundamentals 4
    
    Perfect solution for small business done

--- practicalTypescript/start/typescript/src/tutorial.ts
+++ practicalTypescript/start/typescript/src/tutorial.ts
@@ -1,2 +1,9 @@
-let awesomeName: string = "shakeAndBrake";
-console.log(awesomeName);
+let varString: string = "lasha";
+console.log(varString.toUpperCase());
+
+let varNum: number = 10;
+
+
+let varBool: boolean = varNum >= 18;
+
+varBool = !varBool;
