454c801eec8f40d507217408c652d42b66dcfc1f
lasha <lashatatu@gmail.com>
2024-08-13 22:32:18 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114 - 10 - 05
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 55
    
    Practical Typescript Fundamentals 11
    
    Perfect solution for small business done

--- practicalTypescript/start/typescript/src/tutorial.ts
+++ practicalTypescript/start/typescript/src/tutorial.ts
@@ -1,8 +1,3 @@
-let orderStatus: "processing" | "shipped" | "delivered" = "processing";
-
-orderStatus = "shipped";
-orderStatus = "delivered";
-
-let discount: number | string = 20;
-
-discount = "20%";
+let temperatures: number[] = [10, 14, 17, 21];
+let colors: string[] = ["blue", "yellow", "red"];
+let mixedArray:(number|string)[]=[10,'lasha']
