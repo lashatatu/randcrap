7d8eb4e12a1b8e1ed06693780a81dc0498ae8c63
lasha <lashatatu@gmail.com>
2024-08-19 02:13:21 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114 - 10 - 05
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 55
    
    Practical Typescript Fundamentals 29
    
    Perfect solution for small business done

--- practicalTypescript/start/typescript/src/tutorial.ts
+++ practicalTypescript/start/typescript/src/tutorial.ts
@@ -1,24 +1,28 @@
-type Employee = { id: number; name: string; department: string };
-type Manager = { id: number; name: string; employees: Employee[] };
-
-type Staff = Employee | Manager;
-
-function printStaffDetails(staff: Staff): void {
-  if ("employees" in staff) {
-    console.log(
-      `${staff.name} is an manager in the ${staff.employees.length} employees`
-    );
-  } else {
-    console.log(
-      `${staff.name} is an employee in the ${staff.department} department`
-    );
-  }
-}
-
-const alice: Employee = { id: 1, name: "alice", department: "sales" };
-const steve: Employee = { id: 2, name: "steve", department: "HR" };
-const bob: Manager = { id: 3, name: "bob", employees: [alice, steve] };
 
 
-printStaffDetails(steve)
-printStaffDetails(bob)
+
+/*
+
+//Intersection Types
+
+type Book = { id: number; name: string; price: number };
+type DiscountedBook = Book & { discount: number };
+
+const book1: Book = {
+  id: 1,
+  name: "how to cook a dragon",
+  price: 15,
+};
+const book2: Book = {
+  id: 2,
+  name: "secret life of unicorns",
+  price: 18,
+};
+
+const book3: DiscountedBook = {
+  id: 3,
+  name: "gnomes vs goblins",
+  price: 25,
+  discount: 0.15,
+};
+*/
