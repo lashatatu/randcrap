ffdd260e78e2d636dc83a0fea4e9f61b9e2da503
lasha <lashatatu@gmail.com>
2024-08-19 02:17:42 +0400

    17. Backgrounds, Gradients, & Filters
    CSS 39
    
    Traversy 114 - 10 - 05
    
    DSA
    
    Container With Most Water
    
    100 days of code: day1 completed
    
    Javascript 30 clock done
    
    Typescript 55
    
    Practical Typescript Fundamentals 31
    
    Perfect solution for small business done

--- practicalTypescript/start/typescript/src/tutorial.ts
+++ practicalTypescript/start/typescript/src/tutorial.ts
@@ -3,6 +3,33 @@
 
 /*
 
+//Interface - Fundamentals
+
+interface Book {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+}
+
+const deepWork: Book = {
+  isbn: 9781455586691,
+  title: 'Deep Work',
+  author: 'Cal Newport',
+  genre: 'Self-help',
+};
+
+deepWork.title = 'New Title'; // allowed
+
+
+// deepWork.isbn = 654321; // not allowed
+
+
+*/
+
+
+/*
+
 //Intersection Types
 
 type Book = { id: number; name: string; price: number };
